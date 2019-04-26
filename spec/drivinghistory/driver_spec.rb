2ff81469917ccf3cbd8ab6f7daadc4db4b3b1698
Victor Stasek <stasekvictordev@gmail.com>
2019-04-25 23:30:15 -0400

    implements basic have_attributes tests - red

--- spec/drivinghistory/driver_spec.rb
+++ spec/drivinghistory/driver_spec.rb
@@ -1,4 +1,8 @@
 require 'drivinghistory/driver'
 
-RSpec.describe DrivingHistory::Driver do
+RSpec.describe DrivingHistory::Driver.new("Bob") do
+  it { is_expected.to have_attributes(:name => "Bob") }
+  it { is_expected.to have_attributes(:trips => []) }
+  it { is_expected.to have_attributes(:total_miles_driven => 0) }
+  it { is_expected.to have_attributes(:total_hours_driven => 0) }
 end
