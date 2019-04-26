0201ad048796e338b45c440b04a87a9636f7ea0d
Victor Stasek <stasekvictordev@gmail.com>
2019-04-25 23:58:57 -0400

    tests #add_trip: throwing away Trips with < 5 mph OR > 100 mph - red

--- spec/drivinghistory/driver_spec.rb
+++ spec/drivinghistory/driver_spec.rb
@@ -13,9 +13,22 @@ RSpec.describe DrivingHistory::Driver do
   end
 
   describe '#add_trip' do
-    it "adds a Trip to Driver's trip array" do
-      expect { driver.add_trip('05:20', '05:40', 20) }.to change { driver.trips.length }.from(0).to(1)
+    context "Trip's avg mph > 5 and < 100" do
+      it "adds a Trip to Driver's trip array" do
+        expect { driver.add_trip('05:20', '05:40', 20) }.to change { driver.trips.length }.from(0).to(1)
+      end
+    end
+
+    context "Trip's avg mph < 5" do
+      it "does not add Trip to Driver's trip array" do
+        expect { driver.add_trip('05:00', '09:00', 5) }.to_not change { driver.trips.length }
+      end
+    end
+
+    context "Trip's avg mph > 100" do
+      it "does not add Trip to Driver's trip array" do
+        expect { driver.add_trip('05:00', '09:00', 1000) }.to_not change { driver.trips.length }
+      end
     end
   end
-
 end
