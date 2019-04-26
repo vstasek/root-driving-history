4748c197f11b36f3cdcde3986ee0abf6d782529e
Victor Stasek <stasekvictordev@gmail.com>
2019-04-25 23:12:21 -0400

    implements have_attributes tests for Trip class - red

--- spec/drivinghistory/trip_spec.rb
+++ spec/drivinghistory/trip_spec.rb
@@ -1,5 +1,15 @@
 require 'drivinghistory/trip'
 
-RSpec.describe "Trip" do
+RSpec.describe DrivingHistory::Trip.new("01:00", "04:00", 102) do
+  it { is_expected.to have_attributes(:miles => 102) }
+  it { is_expected.to have_attributes(:hours => 3) }
+  it { is_expected.to have_attributes(:avg_mph => 34) }
 
+  it "should have attribute start time" do
+    expect(subject.start.strftime "%H:%M").to eq("01:00")
+  end
+
+  it "should have attribute stop time" do
+    expect(subject.stop.strftime "%H:%M").to eq("04:00")
+  end 
 end
