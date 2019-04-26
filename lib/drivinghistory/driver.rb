7f0e60cc23cffaad60b9912eba51d1c75513a434
Victor Stasek <stasekvictordev@gmail.com>
2019-04-25 23:34:58 -0400

    implements initialize and properties - green

--- lib/drivinghistory/driver.rb
+++ lib/drivinghistory/driver.rb
@@ -1,4 +1,19 @@
+require_relative 'trip'
+
 module DrivingHistory
   class Driver
+    attr_reader(
+      :name,
+      :trips,
+      :total_miles_driven,
+      :total_hours_driven
+    )
+
+    def initialize driver_name
+      @name = driver_name
+      @trips = []
+      @total_miles_driven = 0.0
+      @total_hours_driven = 0.0
+    end
   end
 end
