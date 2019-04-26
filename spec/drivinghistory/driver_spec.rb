4416c53318cadb52e884ea82caf4a72f94409777
Victor Stasek <stasekvictordev@gmail.com>
2019-04-25 23:51:29 -0400

    describes #add_trip - should add a Trip to the Driver.trip []

--- spec/drivinghistory/driver_spec.rb
+++ spec/drivinghistory/driver_spec.rb
@@ -6,3 +6,16 @@ RSpec.describe DrivingHistory::Driver.new("Bob") do
   it { is_expected.to have_attributes(:total_miles_driven => 0) }
   it { is_expected.to have_attributes(:total_hours_driven => 0) }
 end
+
+RSpec.describe DrivingHistory::Driver do
+  subject(:driver) do
+    described_class.new("Dave")
+  end
+
+  describe '#add_trip' do
+    it "adds a Trip to Driver's trip array" do
+      expect { driver.add_trip('05:20', '05:40', 20) }.to change { driver.trips.length }.from(0).to(1)
+    end
+  end
+
+end
