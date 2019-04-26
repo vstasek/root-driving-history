cba320665c3179460711b4f9f5a2bdbdea2592b9
Victor Stasek <stasekvictordev@gmail.com>
2019-04-26 00:00:34 -0400

    #add_trip now does not add Trips with .avg_mph < 5 OR .avg_mph > 100

--- lib/drivinghistory/driver.rb
+++ lib/drivinghistory/driver.rb
@@ -18,7 +18,14 @@ module DrivingHistory
 
     def add_trip start, stop, miles
       new_trip = DrivingHistory::Trip.new(start, stop, miles)
-      @trips.push new_trip
+      
+      if new_trip.avg_mph < 5 then
+        STDERR.puts "#{name}'s Trip that took place between #{start} and #{stop} was not recorded as the avg mph was less than 5mph."
+      elsif new_trip.avg_mph > 100 then
+        STDERR.puts "#{name}'s Trip that took place between #{start} and #{stop} was not recorded as the avg mph was greater than 100mph."
+      else
+        @trips.push new_trip
+      end
     end
   end
 end
